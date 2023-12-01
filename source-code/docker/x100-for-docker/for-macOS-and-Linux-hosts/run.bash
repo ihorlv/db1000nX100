1d552239a14092a7ed5fe70caf25d7024c1bf3e7
ihorlv <ihorlv@ukr.net>
2023-12-01 21:04:04 +0200

    Release 20231201.2038

--- source-code/docker/x100-for-docker/for-macOS-and-Linux-hosts/run.bash
+++ source-code/docker/x100-for-docker/for-macOS-and-Linux-hosts/run.bash
@@ -12,7 +12,7 @@ fi
 cd "$scriptsRoot"
 
 
-imageTag="tag-20231118.1019"
+imageTag="tag-20231201.2038"
 imageLocal=x100-image-local
 imageLocalPath="$scriptsRoot/${imageLocal}.tar"
 
--- source-code/docker/x100-for-docker/run.cmd
+++ source-code/docker/x100-for-docker/run.cmd
@@ -2,7 +2,7 @@
 setlocal EnableDelayedExpansion 
 set container=x100-container
 
-set imageTag=tag-20231118.1019
+set imageTag=tag-20231201.2038
 set image=ihorlv/x100-image:!imageTag!
 
 set imageLocal=x100-image-local
