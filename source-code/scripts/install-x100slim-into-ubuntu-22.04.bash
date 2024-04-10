a4a5c057d219cb97bb2ec331fbf2f8db793527c3
ihorlv <ihorlv@ukr.net>
2024-04-10 13:00:20 +0300

    Release 20240410.1210 fix

--- source-code/scripts/install-x100slim-into-ubuntu-22.04.bash
+++ source-code/scripts/install-x100slim-into-ubuntu-22.04.bash
@@ -15,7 +15,7 @@ mv -f ./db1000nX100/source-code/*  /root/x100
 rm -rf /root/x100/git
 
 cd    /root/x100/scripts
-/usr/bin/env bash  ./install-x100slim-into-ubuntu-22.04.bash
+/usr/bin/env bash  ./install-x100slim-components-into-ubuntu-22.04.bash
 /usr/bin/env bash  ./fix-permissions.bash
 
 
