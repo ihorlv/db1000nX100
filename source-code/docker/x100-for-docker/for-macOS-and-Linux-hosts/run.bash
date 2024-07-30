05321281301312e37d34d337ed15b8aaa51497bd
ihorlv <ihorlv@x100.vn.ua>
2024-07-30 19:37:03 +0300

    Release 20240730.1909

--- source-code/docker/x100-for-docker/for-macOS-and-Linux-hosts/run.bash
+++ source-code/docker/x100-for-docker/for-macOS-and-Linux-hosts/run.bash
@@ -18,7 +18,7 @@ fi
 cd "$scriptsRoot"
 
 
-imageTag="tag-20240717.1447"
+imageTag="tag-20240730.1909"
 imageLocal=x100-image-local
 imageLocalPath="$scriptsRoot/${imageLocal}.tar"
 
--- source-code/docker/x100-for-docker/run.cmd
+++ source-code/docker/x100-for-docker/run.cmd
@@ -2,7 +2,7 @@
 setlocal EnableDelayedExpansion 
 set container=x100-container
 
-set imageTag=tag-20240717.1447
+set imageTag=tag-20240730.1909
 set image=ihorlv/x100-image:!imageTag!
 
 set imageLocal=x100-image-local
