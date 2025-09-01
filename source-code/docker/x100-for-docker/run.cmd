dcff474c694b895b195d2588a17abc6f35555e5f
ihorlv <ihorlv@x100.vn.ua>
2025-09-01 12:22:38 +0300

    Release 20250901.1139

--- source-code/docker/x100-for-docker/run.cmd
+++ source-code/docker/x100-for-docker/run.cmd
@@ -2,7 +2,7 @@
 setlocal EnableDelayedExpansion 
 set container=x100-container-amd64
 
-set imageTag=tag-20250819.0903
+set imageTag=tag-20250901.1139
 set image=ihorlv/x100-image-amd64:!imageTag!
 
 set imageLocal=x100-image-local
